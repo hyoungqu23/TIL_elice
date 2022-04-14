97b8344bf95690b087562b8eaf61e8feb0a6d2d0
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-04-14 15:49:08 +0900

    0414 javascript practice project tab button

--- W2/THU/script.js
+++ W2/THU/script.js
@@ -1,10 +1,10 @@
 // 1. Scroll Navigation
 // header 내부의 모든 a 태그 선택
-let aTags = document.querySelectorAll("header a");
+let $aTags = document.querySelectorAll("header a");
 
-for (let i = 0; i < aTags.length; i++) {
+for (let i = 0; i < $aTags.length; i++) {
   // 선택한 a 태그 모두에 onclick event handler를 추가
-  aTags[i].onclick = function(e) {
+  $aTags[i].onclick = function(e) {
     // a 태그의 기본 동작 방지
     e.preventDefault();
 
@@ -56,4 +56,35 @@ setInterval(function() {
 
 }, 5000);
 
-// 3.
+// 3. Tab
+// Tab 버튼과 아래 내용을 선택
+let $links = document.querySelectorAll(".tabs-list li a");
+let $items = document.querySelectorAll(".tabs-list li");
+
+// a 태그의 기본 동작 방지
+for (let i = 0; i < $links.length; i++) {
+  $links[i].onclick = function(e) {
+    e.preventDefault();
+  }
+}
+
+// Tab 버튼에 click Event handler 추가
+for (let i = 0; i < $items.length; i++) {
+  $items[i].onclick = function() {
+    // a 태그의 href 속성 값 선택
+    let tabId = this.querySelector("a").getAttribute("href");
+    console.log(this.classList);
+
+    // 모든 Tab 버튼과 내용에서 `active` 클래스를 제거
+    document.querySelectorAll(".tabs-list li, .tabs div.tab").forEach(function(item) {
+      item.classList.remove("active");
+      console.log(item);
+    });
+
+    // 선택한 href와 동일한 Id를 가진 요소에 `active` 클래스 추가
+    document.querySelector(tabId).classList.add("active");
+
+    // 해당 Tab 버튼에도 동일하게 `active` 클래스 추가
+    this.classList.add("active");
+  }
+}
