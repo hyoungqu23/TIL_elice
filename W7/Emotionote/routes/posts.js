1180ce58cfe91d63d11e1aa120a23938040ccb3c
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-05-17 08:53:55 +0900

    0517 emotionotes POST method

--- W7/Emotionote/routes/posts.js
+++ W7/Emotionote/routes/posts.js
@@ -8,4 +8,23 @@ router.get('/', (req, res, next) => {
   }
 })
 
+router.post('/', (req, res, next) => {
+  const { title, content } = req.body;
+  
+  try {
+
+    await Post.create({
+      title,
+      content,
+    }); // 데이터 생성
+
+    res.redirect('/');  // root page로 이동
+    
+    // 해당 게시글로 이동하기 위해서는 생성한 데이터를 posts로 받고, shortId를 활용해 이동할 수 있다.
+
+  } catch (err) {
+
+  }
+})
+
 module.exports = router;
