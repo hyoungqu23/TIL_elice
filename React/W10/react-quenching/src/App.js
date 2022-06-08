8372b290748a005d8e87236ec669e9e48ff4c567
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-08 14:22:58 +0900

    🎨 React.js State

--- React/W10/react-quenching/src/App.js
+++ React/W10/react-quenching/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 
@@ -29,7 +30,7 @@ function Nav(props) {
           href={`/read/${id}`}
           onClick={(e) => {
             e.preventDefault();
-            props.onClick();
+            props.onClick(id); // Closure로 인해 이벤트 핸들러에 id를 전달하는 방법(Destructuring으로 인해 id만 해도 된다. 안한다면 event.id로 전달해야 한다. 물론 map의 id를 전달해야 한다.)
           }}
         >
           {title}
@@ -55,6 +56,9 @@ function Article(props) {
 }
 
 function App() {
+  // let mode = 'READ'; // 단순 지역변수
+  let [mode, setMode] = useState('WELCOME');
+
   const topics = [
     { id: 1, title: 'HTML', body: 'HTML is Hyper Text Markup Language' },
     { id: 2, title: 'CSS', body: 'CSS is Cascading Style Sheets' },
@@ -72,20 +76,32 @@ function App() {
     console.log('Update Button');
   };
 
+  // mode의 값에 따라 content가 변경된다.
+  let content = <Article title="Welcome" body="Hello, React!" />;
+  if (mode === 'WELCOME') {
+    content = <Article title="Welcome" body="Welcome, React!" />;
+  } else if (mode === 'READ') {
+    content = <Article title="Read" body="Read, React!" />;
+  }
+
   return (
     <div className="App">
       <Header
         onSelect={() => {
           console.log('Header Click');
+          // mode = 'WELCOME'; // mode를 변경한다. 다만, 렌더링되지 않는다. 따라서 App 함수가 다시 호출되고, 화면에 다시 렌더링하게끔 해야한다. 하지만 App()을 통해 강제로 호출하더라도 상단에서 기본값으로 적용되어 변화가 없다. -> 이렇게 하면 안되고, useState를 사용해야 한다.(상태값 변경 시 Component 재실행)
+          setMode('WELCOME');
         }}
       />
       <Nav
         data={topics}
-        onClick={() => {
-          console.log('Nav Click');
+        onClick={(id) => {
+          console.log('Nav Click', id); // 어떤 것을 클릭했는지 알 수 있어야 한다. -> id를 인자로 받아야 함
+          // mode = 'READ';
+          setMode('READ');
         }}
       />
-      <Article title="Welcome" body="Hello, React!" />
+      {content}
       <ButtonGroup variant="contained">
         <Button onClick={handleCreateBtn}>Create</Button>
         <Button onClick={handleUpdateBtn}>Update</Button>
@@ -98,8 +114,6 @@ function App() {
       >
         Delete
       </Button>
-      <img src="./logo.svg" alt="" />
-      <a href="http://info.cern.ch">WEB</a>
     </div>
   );
 }
