fc661d4c4390e46f21699c00711c123dfa27f2d9
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-10 22:18:39 +0900

    [lecture] ListView component style w external CSS

--- React/W10/react-todo/src/components/ListView.js
+++ React/W10/react-todo/src/components/ListView.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import './ListView.css';
 
 const ListView = ({ todos, onComplete, onRemove }) => {
   return (
-    <div>
+    <div className="listview">
       <ol>
         {todos.map((todo, index) => {
           return (
-            <li key={todo.key}>
+            <li key={todo.key} className={todo.isCompleted ? 'completed' : ''}>
               <span>{todo.value}</span>
               <button type="button" onClick={() => onComplete(index)}>
                 완료
               </button>
-              <button type="button" onClick={() => onRemove(index)}>
+              <button
+                className="remove"
+                type="button"
+                onClick={() => onRemove(index)}
+              >
                 삭제
               </button>
             </li>
