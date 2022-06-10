39a4df50774220b87f5a1c7c121a327bded2b21e
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-10 22:13:51 +0900

    [lecture] Form component Style with inline-css

--- React/W10/react-todo/src/components/Form.js
+++ React/W10/react-todo/src/components/Form.js
@@ -10,15 +10,44 @@ const Forms = (props) => {
   };
 
   return (
-    <form onSubmit={handleSubmitBtn}>
+    <form
+      onSubmit={handleSubmitBtn}
+      style={{
+        backgroundColor: '#ffffff',
+        borderRadius: 16,
+        margin: 16,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
       <input
         type="text"
         value={inputValue}
         onChange={(event) => {
           setInputValue(event.target.value);
         }}
+        style={{
+          flex: 1,
+          border: 'none',
+          color: '#000000',
+          padding: '6px 12px',
+          backgroundColor: 'transparent',
+        }}
       />
-      <button type="submit">등록</button>
+      <button
+        type="submit"
+        style={{
+          border: 'none',
+          borderRadius: 16,
+          backgroundColor: '#3ab6bc',
+          color: '#ffffff',
+          cursor: 'pointer',
+          padding: '8px 16px',
+        }}
+      >
+        등록
+      </button>
     </form>
   );
 };
