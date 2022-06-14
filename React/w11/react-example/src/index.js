cf9488697799fe2431ebdcca973a8453df1dcc32
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-14 21:28:38 +0900

    [0614] add all Components

--- React/w11/react-example/src/index.js
+++ React/w11/react-example/src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import RegisterForm from './ResisterForm';
+import MathProblem from './MathProblem';
 import reportWebVitals from './reportWebVitals';
+import Register from './Register';
+import Calculator from './Calculator';
+import GlobalStyle from './GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
+    <RegisterForm />
+    <MathProblem />
+    <Register />
+    <Calculator />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
