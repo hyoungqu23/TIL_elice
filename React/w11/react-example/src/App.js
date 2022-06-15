f5264614e7817b616a8b0a05b0e630f09ca6ceac
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-15 14:26:42 +0900

    [0615] ajax Create 구현(Read 에러 발생)

--- React/w11/react-example/src/App.js
+++ React/w11/react-example/src/App.js
@@ -66,8 +66,6 @@ function App() {
   const [topics, setTopics] = useState([
     { id: 1, title: 'HTML5', body: 'HTML5 is ...' },
     { id: 2, title: 'CSS3', body: 'CSS3 is ...' },
-    { id: 3, title: 'JavaScript', body: 'JavaScript is ...' },
-    { id: 4, title: 'React.js', body: 'React.js is ...' },
   ]);
   const [nextId, setNextId] = useState(5);
   const navigate = useNavigate();
@@ -93,10 +91,7 @@ function App() {
           path="/"
           element={<Article title="Welcome" body="Hello, WEB!"></Article>}
         />
-        <Route
-          path="/Create"
-          element={<Create onCreate={handleOnCreate()} />}
-        />
+        <Route path="/Create" element={<Create onCreate={handleOnCreate} />} />
         <Route path="/read/:id" element={<Read topics={topics}></Read>} />
       </Routes>
       {/* 버튼 UI 수정 방법 1 - 가능은 하지만, 중복이 심하고, URL parameter를 가져올 수 없다. */}
@@ -158,26 +153,42 @@ function App() {
     </div>
   );
 
-  function handleOnCreate() {
-    return (title, body) => {
-      // State가 객체인 경우, immutable하게 처리해야 한다.
-      const newTopic = {
-        // id: topics.length + 1, // 삭제 기능으로 인해 uniqueness 훼손 가능성 존재하여 불가능
-        id: nextId,
+  async function handleOnCreate(title, body) {
+    // ajax
+    const response = await fetch('http://localhost:3333/topics', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        // id: nextId, 자동 생성으로 필요 없음
         title,
         body,
-      };
-      // 객체의 불변성 유지로 인해 push 메서드는 렌더링이 되지 않음(기존 배열 객체와 달라야 렌더링이 된다.)
-      // Hot Module Replacement(HMR), Reloading를 사용하여 불변성 유지를 위해 setState를 사용하여 배열을 변경하는 방법
-      setTopics((currentTopics) => [...currentTopics, newTopic]);
+      }),
+    });
 
-      // 작성한 글 보기로 전환
-      setId(nextId);
-      setMode('READ');
+    const data = await response.json();
 
-      // 다음 추가를 위한 준비
-      setNextId((currentNextId) => currentNextId + 1);
-    };
+    // 방금 생성한 글로 사용자 이동
+    navigate(`/read/${data.id}`);
+
+    // // State가 객체인 경우, immutable하게 처리해야 한다.
+    // const newTopic = {
+    //   // id: topics.length + 1, // 삭제 기능으로 인해 uniqueness 훼손 가능성 존재하여 불가능
+    //   id: nextId,
+    //   title,
+    //   body,
+    // };
+    // // 객체의 불변성 유지로 인해 push 메서드는 렌더링이 되지 않음(기존 배열 객체와 달라야 렌더링이 된다.)
+    // // Hot Module Replacement(HMR), Reloading를 사용하여 불변성 유지를 위해 setState를 사용하여 배열을 변경하는 방법
+    // setTopics((currentTopics) => [...currentTopics, newTopic]);
+
+    // // 작성한 글 보기로 전환
+    // setId(nextId);
+    // setMode('READ');
+
+    // // 다음 추가를 위한 준비
+    // setNextId((currentNextId) => currentNextId + 1);
   }
 
   function handleUpdateButton() {
