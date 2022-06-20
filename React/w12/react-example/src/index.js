936200e93c1d24b4f51f0320a1820506a6381284
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-20 10:37:54 +0900

    [0620] DB 수정

--- React/w12/react-example/src/index.js
+++ React/w12/react-example/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 import { Reset } from 'styled-reset';
 
 import App from './App';
