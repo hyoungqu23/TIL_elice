f7412f03e3b74bc4f450573ccd55c7a77ba1f70e
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-20 14:15:34 +0900

    [0620] useState를 활용한 Counter Component 구현

---
+++ React/w12/react-example/src/components/Reducer.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <div style={{ fontSize: '50px' }}>{count}</div>
+      <StyledButton
+        value="-"
+        onClick={() => {
+          setCount((count) => count - 1);
+        }}
+      >
+        -
+      </StyledButton>
+      <StyledButton
+        value="0"
+        onClick={() => {
+          setCount(0);
+        }}
+      >
+        0
+      </StyledButton>
+      <StyledButton
+        value="+"
+        onClick={() => {
+          setCount((count) => count + 1);
+        }}
+      >
+        +
+      </StyledButton>
+    </div>
+  );
+}
+
+export default App;
+
+const StyledButton = styled.button`
+  background-color: #8af;
+  border-radius: 5px;
+  width: 50px;
+  height: 50px;
+  font-size: 30px;
+`;
--- React/w12/react-example/src/index.js
+++ React/w12/react-example/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 // import App from './App';
-import App from './components/Context';
+// import App from './components/Context';
+import App from './components/Reducer';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
