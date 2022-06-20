9a67acb1e0bb164b6c179f4fa1def9ef955684a6
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-20 14:07:58 +0900

    [0620] Context API 활용

--- React/w12/react-example/src/components/Context.js
+++ React/w12/react-example/src/components/Context.js
@@ -1,5 +1,60 @@
-const App = () => {
-  return <div>Context</div>;
+import { createContext, useContext } from 'react';
+import './Context.css';
+
+const themeDefault = {
+  color: 'lightgreen',
+  border: '5px dashed lightgreen',
 };
 
+const context = createContext(themeDefault);
+
+function App() {
+  return (
+    <>
+      <h1>0</h1>
+      <Child1></Child1>
+    </>
+  );
+}
+
+function Child1() {
+  const theme = useContext(context);
+  return (
+    <div style={theme}>
+      <h1>1</h1>
+      <Child2></Child2>
+    </div>
+  );
+}
+
+function Child2() {
+  const theme = useContext(context);
+  return (
+    <div style={theme}>
+      <h1>2</h1>
+      <context.Provider value={{ ...theme, border: '5px dotted skyblue' }}>
+        <Child3></Child3>
+      </context.Provider>
+    </div>
+  );
+}
+
+function Child3() {
+  const theme = useContext(context);
+  return (
+    <div style={theme}>
+      <h1>3</h1>
+      <Child4></Child4>
+    </div>
+  );
+}
+
+function Child4() {
+  const theme = useContext(context);
+  return (
+    <div style={theme}>
+      <h1>4</h1>
+    </div>
+  );
+}
 export default App;
