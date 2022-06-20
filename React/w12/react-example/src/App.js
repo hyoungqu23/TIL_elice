0dcc707844d3d6f6d7d8b16ad2a5ff96dec735e5
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-20 10:44:42 +0900

    [0620] 동적으로 Nav item 추가

--- React/w12/react-example/src/App.js
+++ React/w12/react-example/src/App.js
@@ -1,3 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import { Reset } from 'styled-reset';
+
 const Header = () => {
   return (
     <header>
@@ -8,19 +11,41 @@ const Header = () => {
   );
 };
 
-const NavBar = () => {
+const NavBar = ({ menus }) => {
+  const menuItems = menus.map((menu) => {
+    return (
+      <li key={menu.id}>
+        <a href={`/read/${menu.id}`}>{menu.title}</a>
+      </li>
+    );
+  });
+
   return (
     <nav>
-      <ul></ul>
+      <ul>{menuItems}</ul>
     </nav>
   );
 };
 
 function App() {
+  const [topics, setTopics] = useState([]);
+
+  const refreshTopics = async () => {
+    const response = await fetch('http://localhost:3333/topics');
+    const result = await response.json();
+    setTopics(result);
+  };
+
+  // useEffect의 콜백 함수에는 async를 직접 사용하지 못하므로, 따로 함수를 생성 후 호출해야 한다.
+  useEffect(() => {
+    refreshTopics();
+  }, []); // 최초 1회 실행
+
   return (
     <div className="App">
+      <Reset />
       <Header />
-      <NavBar />
+      <NavBar menus={topics} />
     </div>
   );
 }
