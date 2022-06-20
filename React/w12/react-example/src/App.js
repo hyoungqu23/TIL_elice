0d85fae9464301ae48b0c13f6333472154f09a52
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-20 10:20:09 +0900

    [0620] Header Component, NavBar Component 추가

--- React/w12/react-example/src/App.js
+++ React/w12/react-example/src/App.js
@@ -1,5 +1,28 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+};
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul></ul>
+    </nav>
+  );
+};
+
 function App() {
-  return <div className="App">Hello World.</div>;
+  return (
+    <div className="App">
+      <Header />
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
