8c3035ed23c9754f5f08605cdafb9cecba0f53c5
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-29 11:42:34 +0900

    [0629] App Component
    useEffect를 활용해 처음 렌더링될 때 데이터 불러오기 설정

--- React/w13/react-app/src/App.js
+++ React/w13/react-app/src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/UI/Header';
 import Nav from './components/UI/Nav';
 import Welcome from './components/ContentsList/Welcome';
 import Read from './components/ContentsList/Read';
 
 const App = () => {
+  const [topics, setTopics] = useState([]);
+
+  const getTopicsData = async () => {
+    const url = 'http://localhost:3333/topics';
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setTopics(data);
+  };
+
+  useEffect(() => {
+    getTopicsData();
+  }, []);
+
   return (
     <div>
       <Header />
-      <Nav />
+      <Nav topics={topics} />
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/read/1" element={<Read />} />
