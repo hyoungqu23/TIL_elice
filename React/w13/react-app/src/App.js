4f96217613e8182c7b57428ac5e3868df1407813
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-29 11:08:30 +0900

    [0629] Welcome Component 생성 및 추가

--- React/w13/react-app/src/App.js
+++ React/w13/react-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/UI/Header';
 import Nav from './components/UI/Nav';
+import Welcome from './components/ContentsList/Welcome';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Nav />
+      <Welcome />
     </div>
   );
 };
---
+++ React/w13/react-app/src/components/ContentsList/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      <p>Hello, WEB!</p>
+    </article>
+  );
+};
+
+export default Welcome;
