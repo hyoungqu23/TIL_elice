0f5739018f7d75297ea46c65abc85e7e774975f9
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-07-01 13:40:20 +0900

    [add] add svg image

---
+++ React/w13/ui03/src/icons/Chart.js
@@ -0,0 +1,20 @@
+const Chart = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M8.33333 16.6666H11.6667V3.33331H8.33333V16.6666ZM3.33333 16.6666H6.66667V9.99998H3.33333V16.6666ZM13.3333 7.49998V16.6666H16.6667V7.49998H13.3333Z"
+        fill="#151618"
+      />
+    </svg>
+  );
+};
+
+export default Chart;
