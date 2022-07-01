f6f158f9cb4fb2ee2b0e1528d3468a4169c2a3c8
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-07-01 10:34:14 +0900

    [feat] App Compo.
    - styled-components로 Container 중앙 정렬 설정

--- React/w13/ui03/src/App.js
+++ React/w13/ui03/src/App.js
@@ -3,6 +3,10 @@ import CourseCard from './components/Course/CourseCard.js';
 
 const Container = styled.div`
   background-color: #f0f1f3;
+
+  display: flex;
+  height: 100vh;
+  align-items: center;
 `;
 
 export default function App() {
